95b300428953509f6ef08ffe417837849da6e073
Astrido <73673156+asterd-og@users.noreply.github.com>
2022-01-12 09:36:15 -0300

    Just to add folders

---
+++ Bright/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;                                                  
+using static System.Console;
+
+namespace Bright {
+        public class Program {
+                public static void Main() {
+                        WriteLine("Hello World! This is the"); 
+                        WriteLine("main file to the Bright Programming Language");                 
+		}
+	}
+}
