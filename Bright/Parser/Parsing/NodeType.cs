6842bbfc80475c18a295098d914c45184c351922
Astrido <73673156+asterd-og@users.noreply.github.com>
2022-01-25 10:14:52 -0300

    Create NodeType.cs

---
+++ Bright/Parser/Parsing/NodeType.cs
@@ -0,0 +1,10 @@
+namespace Bright.Parser.Parsing {
+    public enum NodeType {
+        EOF=0,
+        vardef=1,
+        varmod=2,
+        functiondef=3,
+        methoddef=4,
+        call=5
+    }
+}
