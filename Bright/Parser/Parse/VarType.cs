13f574577daa243a3e7f7f13cd5a65699f392dc4
Astrido <73673156+asterd-og@users.noreply.github.com>
2022-01-25 09:52:25 -0300

    Delete Bright directory

--- Bright/Parser/Parse/VarType.cs
+++
@@ -1,7 +0,0 @@
-namespace  Bright.Parser.Parse {
-    public enum VarType {
-        STRING=0,
-        INT=1,
-        FLOAT=2
-    }
-}
--- Bright/run.sh
+++
@@ -1,3 +0,0 @@
-nasm -f elf64 file.asm
-ld.lld -s -o file file.o
-./file
